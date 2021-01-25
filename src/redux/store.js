94bb98feb17b73f8b2eadb6eeceb76686ffa2a9c
Yuriev Yuriy <yuriev1986@gmail.com>
2021-01-25 16:21:16 +0200

    add local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import reducer from './reducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+// const rootReducer = combineReducers(reducer);
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 // const store = configureStore(reducer, composeWithDevTools());
 const store = configureStore({
-  reducer,
+  reducer: persistedReducer,
   devTools: process.env.NODE_ENV === 'development',
 });
 
-export default store;
+const persistor = persistStore(store);
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { store, persistor };
